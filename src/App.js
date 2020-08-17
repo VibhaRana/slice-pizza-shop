2302c702eefb084ebe4ae31f1456cce4d70ff370
Vibha Rana Arora <vibha@MacBook-Pro.local>
2020-08-17 13:57:19 -0700

    Added styled components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
-
+import { createGlobalStyle } from 'styled-components'
 import './App.css';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Open Sans', sans-serif;
+  }
+  h1, h2, h3 {
+    font-family: 'Righteous', cursive;
+  }
+`
+
+
 
 function App() {
   return (
+    
     <div className="App">
-      
+      <> 
+      <GlobalStyle />
+     <h1>Sliceline</h1>
+      </>
     </div>
+   
   );
 }
 
